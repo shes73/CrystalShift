657e85fff62002ae69512ee605924cf6e3853500
Ilona Isupova <107842759+shes73@users.noreply.github.com>
2024-11-26 19:09:56 +0700

    Delete CrystalShift_main_module directory

--- CrystalShift_main_module/xyz_parser.h
+++
@@ -1,8 +0,0 @@
-#ifndef XYZ_PARSER_H
-#define XYZ_PARSER_H
-
-void inverse_matrix(double transformation_matrix[3][3], double inversed_transformation_matrix[3][3]);
-
-int parse_xyz(const char *filename, int xyz_format);
-
-#endif
--- CrystalShift_main_module/xyz_writer.h
+++
@@ -1,8 +0,0 @@
-#ifndef XYZ_WRITER_H
-#define XYZ_WRITER_H
-
-#include "structures.h"
-
-int write_xyz(const char *filename, const Structure *structure, int xyz_format);
-
-#endif
