21eb4e3343b66bd2102cebf6c0d94bd693070a28
Ilona Isupova <107842759+shes73@users.noreply.github.com>
2025-02-04 21:49:30 +0700

    Update basis_changer.h

--- main/crystalshift/basis_changer.h
+++ main/crystalshift/basis_changer.h
@@ -2,6 +2,7 @@
 #define BASIS_CHANGER_H
 
 void get_the_transformation_matrix(double transformation_matrix[3][3]);
+void swap_rows(double matrix[3][3], double inverse[3][3], int row1, int row2);
 void inverse_transformation_matrix(double transformation_matrix[3][3], double inversed_transformation_matrix[3][3]);
 void calculate_new_basis_matrix(double transformation_matrix[3][3], double new_basis_matrix[3][3], double new_cell_lengths[3], double new_cell_angles[3]);
 void calculate_new_coords(double inversed_transformation_matrix[3][3]);
